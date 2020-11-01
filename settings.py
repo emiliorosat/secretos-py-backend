b63e90f4e3817b4e85e862af42a6fea28b9e0d0a
Emilio Rosa <me@emiliort.com>
2020-11-01 03:46:53 -0400

    fixing environ

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 print(os.environ.keys())
 
 
-jwtSecret = os.environ("JWT_SECRET")
+jwtSecret = os.environ["JWT_SECRET"]
 
 if(jwtSecret == None):
     jwtSecret = os.getenv("JWT_SECRET")
