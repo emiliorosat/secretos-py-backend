0b02c2d1e228de4032625558ce32d59b1c875934
Emilio Rosa <42384263+emiliorosat@users.noreply.github.com>
2020-11-01 03:41:49 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,14 +1,14 @@
-from dotenv import load_dotenv
-import bcrypt
+#from dotenv import load_dotenv
+#import bcrypt
 import os
 
-load_dotenv()
+#load_dotenv()
 
-print(os.environ.keys())
+#print(os.environ.keys())
 
 
 jwtSecret = os.environ("JWT_SECRET")
 
-if(jwtSecret == None):
-    jwtSecret = os.getenv("JWT_SECRET")
+#if(jwtSecret == None):
+#    jwtSecret = os.getenv("JWT_SECRET")
 
